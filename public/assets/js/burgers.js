da28bfdeffe0d37b28ae93562bf42391da6327da
fcarlone <frankcarlone@gmail.com>
2019-05-12 17:31:34 -0400

    Style devoured list

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,20 @@
-$(function () {
-  $("#add-burger").on("submit", function (event) {
-    event.preventDefault();
+// $(function () {
+//   $("#add-burger").on("submit", function (event) {
+//     event.preventDefault();
 
-    let addBurger = {
-      burger_name: $("#add-burger [name=burger]").val().trim()
+//     let addBurger = {
+//       burger_name: $("#add-burger [name=burger]").val().trim()
 
-    }
-    console.log("add new burger", addBurger)
+//     }
+//     console.log("add new burger", addBurger)
 
-    // Send the POST requestl
-    $.post("/api/burgers", addBurger, function (data) {
-      console.log('burger added', data);
-      // Reload the page to update burger list
-      location.reload();
-    })
+//     // Send the POST requestl
+//     $.post("/api/burgers", addBurger, function (data) {
+//       console.log('burger added', data);
+//       // Reload the page to update burger list
+//       location.reload();
+//     })
 
-  });
+//   });
 
-});
+// });
