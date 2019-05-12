9a75abccdc11c301b5a0e3a2ba41f7fe64e56849
fcarlone <frankcarlone@gmail.com>
2019-05-12 08:33:44 -0400

    Refactor server.js file

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
-// Import routes
-const routes = require("./controllers/burgers_controller");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-// Give server access to routes
-app.use(routes);
-
-// Serve static content
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Middleware to parse application body as JSON
+// Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Config handlebars
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Add listener
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
-  console.log(`Listening on PORT: ${PORT}`)
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
 });
