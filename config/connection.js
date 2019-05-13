11fd77d3c220e46174e457928a8dec39515d2356
fcarlone <frankcarlone@gmail.com>
2019-05-13 11:37:39 -0400

    Config connection.js file for JAWS_DB add-on

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
+let connection;
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Fodder165*",
-  database: "burgers_db"
-})
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Fodder165*",
+    database: "burgers_db"
+  });
+};
 
 // Make connection
 connection.connect(function (err) {
